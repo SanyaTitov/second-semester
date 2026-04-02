d2ec0a7fd400d57a9a0e22492973d4e408ea48e9
SanyaTitov <alexsandrvladimirovih2008@gmail.com>
2026-04-02 13:02:28 +0500

    try complete week 11

--- week-11-typescript-basis/UtilityTypes.ts
+++ week-11-typescript-basis/UtilityTypes.ts
@@ -4,4 +4,31 @@ interface User {
     email?: string;
     age: number;
 }
-type UpdateUserRequest = Partial<User>;
+type UserEditForm = Partial<User> & { id: User['id']};
+
+function processData(input: string): SuccessResponse | ErrorResponse{
+    console.log('привет')
+}
+
+type isProcessSuccess = SuccessResponse | ErrorResponse
+
+interface SuccessResponse {
+    data: 'User';
+    status: 'ok';
+}
+
+interface ErrorResponse {
+    data: 'User';
+    status: 'not ok';
+}
+
+declare function processData(): Promise<isProcessSuccess>;
+
+function isSuccessResponse(response: ReturnType<typeof processData>): response is SuccessResponse{
+    return response.status === 'ok';
+}
+
+const result: isProcessSuccess = await processData();
+if (isProccess(result)) {
+    console.log(result.data.name);
+}
